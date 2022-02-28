16bdc370f134d2568d76116012a7f5e94801557d
Berkay Özgün <57600835+Derv1s@users.noreply.github.com>
2022-02-28 18:25:28 +0300

    Update index.py

--- oop_game/index.py
+++ oop_game/index.py
@@ -2,7 +2,7 @@ from Player import Player
 from HumanPlayer import HumanPlayer
 from DuelField import DuelField
 
-player_1 = HumanPlayer("Ugur")
+player_1 = HumanPlayer("Berkay")
 player_2 = Player("AI_0")
 
 game = DuelField(player_1, player_2)
